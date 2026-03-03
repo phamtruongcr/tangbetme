0f47b151b227d743813776a481318a52d0f53466
Phạm Trường <52502523+phamtruongcr@users.noreply.github.com>
2026-03-03 14:16:06 +0700

    init

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Giáng sinh rồi, bé có biết không!',
+    introDesc: `Như! bé đọc được dòng này,bé phải mua thuốc đau lưng vì anh làm món quà nhỏ này cho bé đó,anh dởn đấy, happy nha bé:3 `,
+    btnIntro: 'Bé bấm vào đi!',
+    title: 'Anh chân thành muốn trao bé một xíu happy ngọt ngào này 🥳',
+    desc: 'Anh thấy được chiếc lùn, đóa là bé:3 ',
+    btnYes: 'Vâng <33',
+    btnNo: 'Bé không biết :3',
+    question:' Bé muốn gửi gì cho anh hông!',
+    btnReply: 'Gửi luôn anh <3',
+    reply: 'Anh dễ thương và cute hihi <33333333',
+    mess: 'Anh biết bé nói vậy mà, anh chân thành cảm ơn bé:3',
+    messDesc: 'Bấm vào bên dưới đi bé, đừng hồi hộp, anh cũng hồi hộp nữa hihi',
+    btnAccept: 'Bấm nè tin tin <3 ',
+    messLink: 'sinhnhatem-ne-master/happy-birthday-master/index.html' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
